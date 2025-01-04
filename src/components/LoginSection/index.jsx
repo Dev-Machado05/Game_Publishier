9d2cdfa333bae123f569d5c9f6a12180d9dc10a3
Lucas Machado <lucasmachadoale4@gmail.com>
2025-01-03 22:55:39 -0300

    Fix: Ajusted the Login Page elements

--- src/components/LoginSection/index.jsx
+++ src/components/LoginSection/index.jsx
@@ -6,17 +6,19 @@ export default function LoginSection() {
       <main className="LoginSectionContent">
         <h1>Login</h1>
         <section>
-          <label>Digite</label>
+          <label>Digite o seu email:</label>
           <input type="text" name="Email" placeholder="Email:" />
         </section>
         <section>
-          <label>Digite</label>
-          <input type="password" name="Password" placeholder="Senha:"/>
+          <label>Digite a sua senha:</label>
+          <input type="password" name="Password" placeholder="Senha:" />
         </section>
         <a href="">Esqueceu a senha:</a>
         <a href="/SignUp">Criar uma conta:</a>
-        <button>Back</button>
-        <button>Login</button>
+        <div className="ButtonContainer">
+          <button className="BackButton">Back</button>
+          <button className="NextButton">Login</button>
+        </div>
       </main>
     </div>
   );
